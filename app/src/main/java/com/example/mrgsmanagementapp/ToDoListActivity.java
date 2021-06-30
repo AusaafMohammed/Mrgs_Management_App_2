c9bee614fe691fd02e22f6d7aedd8c8a5f0c0b2e
Ausaaf Mohammed <17573@students.mrgs.school.nz>
2021-06-30 19:43:12 +1200

    To do list done uptil Retrieve layout

--- app/src/main/java/com/example/mrgsmanagementapp/ToDoListActivity.java
+++ app/src/main/java/com/example/mrgsmanagementapp/ToDoListActivity.java
@@ -1,20 +1,48 @@
 package com.example.mrgsmanagementapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class ToDoListActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private FloatingActionButton floatingActionButton;
+
+    private DatabaseReference reference;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private String onlineUserID;
+
+    private ProgressDialog loader;
+
+    private String key = "";
+    private String task;
+    private String description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +56,15 @@ public class ToDoListActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(linearLayoutManager);
 
+        loader = new ProgressDialog(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        onlineUserID = mUser.getUid();
+        reference = FirebaseDatabase.getInstance().getReference().child("tasks").child(onlineUserID);
+
         floatingActionButton = findViewById(R.id.fab);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addTask();
-            }
-        });
+        floatingActionButton.setOnClickListener(v -> addTask());
 
     }
 
@@ -45,8 +75,53 @@ public class ToDoListActivity extends AppCompatActivity {
         View myView = inflater.inflate(R.layout.input_file, null);
         myDialog.setView(myView);
 
-        AlertDialog dialog = myDialog.create();
+        final AlertDialog dialog = myDialog.create();
         dialog.setCancelable(false);
+
+        final EditText task = myView.findViewById(R.id.task);
+        final EditText description = myView.findViewById(R.id.description);
+        Button save = myView.findViewById(R.id.btn_save);
+        Button cancel = myView.findViewById(R.id.btn_cancel);
+
+        cancel.setOnClickListener(v -> dialog.dismiss());
+
+        save.setOnClickListener(v -> {
+            String mTask = task.getText().toString().trim();
+            String mDescription = description.getText().toString().trim();
+            String id = reference.push().getKey();
+            String date = DateFormat.getDateInstance().format(new Date());
+
+            if (TextUtils.isEmpty(mTask)){
+                task.setError("Task Required");
+                return;
+            }
+            if (TextUtils.isEmpty(mDescription)){
+                description.setError("Description Required");
+                return;
+            }else {
+                loader.setMessage("Adding your data");
+                loader.setCanceledOnTouchOutside(false);
+                loader.show();
+
+                Model model = new Model(mTask,mDescription,id,date);
+                reference.child(id).setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(ToDoListActivity.this, "Task has been inserted successfully", Toast.LENGTH_SHORT).show();
+                            loader.dismiss();
+                        }else {
+                            String error = task.getException().toString();
+                            Toast.makeText(ToDoListActivity.this, "Failed: " + error, Toast.LENGTH_SHORT).show();
+                            loader.dismiss();
+                        }
+                    }
+                });
+            }
+            dialog.dismiss();
+        });
         dialog.show();
     }
+
+
 }
