c02e3ca1263685224a08babc158c15f6bc8b6a4d
Ausaaf Mohammed <17573@students.mrgs.school.nz>
2021-09-24 15:36:26 +1200

    7th scholorship update, Calendar added back, also included the alternative

--- app/src/main/java/com/example/mrgsmanagementapp/PlannerActivity.java
+++ app/src/main/java/com/example/mrgsmanagementapp/PlannerActivity.java
@@ -1,10 +1,8 @@
 package com.example.mrgsmanagementapp;
-
 //These are the imports for PlannerActivity.java
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 //import android.content.Intent;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,27 +10,78 @@ import android.view.View;
 //import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-
-public class PlannerActivity extends AppCompatActivity {
-
+import static com.example.mrgsmanagementapp.CalendarUtils.daysInMonthArray;
+import static com.example.mrgsmanagementapp.CalendarUtils.monthYearFromDate;
+public class PlannerActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener
+{
+    //  These are for creating variables
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
     @Override
-    protected void onCreate (Bundle savedInstanceState){
-        super.onCreate (savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planner);
+        initWidgets();
+        CalendarUtils.selectedDate = LocalDate.now();
+        setMonthView();
     }
 
-    public void openApp(View view){
+    public void openApp(View view) {
         Intent intent = getPackageManager().getLaunchIntentForPackage("com.android.calendar");
         if(intent != null){
             startActivity(intent);
         } else{
-            Toast.makeText(PlannerActivity.this, "There is no package", Toast.LENGTH_LONG).show();
+            Toast.makeText(PlannerActivity.this, "There is no Package", Toast.LENGTH_SHORT).show();
         }
     }
 
+    //  Creating initWidgets method
+    private void initWidgets() {
+//      defining the variables
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+    }
+    //  Creating setMonthView method
+    private void setMonthView()
+    {
+//      This part of the code is for the design of calendar with 7 grids and selected date
+        monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalendarUtils.selectedDate);
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+    //  This part of the code is for viewing Previous Month from CalendarUtils
+    public void previousMonthAction(View view)
+    {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusMonths(1);
+        setMonthView();
+    }
+    //  This part of the code is for viewing Next Month from CalendarUtils
+    public void nextMonthAction(View view)
+    {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusMonths(1);
+        setMonthView();
+    }
+    @Override
+    public void onItemClick(int position, LocalDate date)
+    {
+//      If date is selected
+        if (date != null)
+        {
+            CalendarUtils.selectedDate = date;
+            setMonthView();
+        }
+    }
+
+    //  This part of the code is for starting WeekViewActivity
+    public void weeklyAction(View view)
+    {
+        startActivity(new Intent(this, WeekViewActivity.class));
+    }
 }
---
+++ app/src/main/java/com/example/mrgsmanagementapp/WeekViewActivity.java
@@ -0,0 +1,98 @@
+package com.example.mrgsmanagementapp;
+
+//These are the imports for WeekViewActivity.java
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static com.example.mrgsmanagementapp.CalendarUtils.daysInWeekArray;
+import static com.example.mrgsmanagementapp.CalendarUtils.monthYearFromDate;
+
+public class WeekViewActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener
+{
+    //  These are for creating variables
+    private ListView eventListView;
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week_view);
+        initWidgets();
+        setWeekView();
+    }
+
+    //  Creating initWidgets method
+    private void initWidgets() {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+        eventListView = findViewById(R.id.eventListView);
+    }
+
+    //  Creating setWeekView method
+    private void setWeekView()
+    {
+        monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> days = daysInWeekArray(CalendarUtils.selectedDate);
+
+//      This part of the code is for the amount of cells (grids) in week view - I chose 7 because of 7 days in week
+        CalendarAdapter calendarAdapter = new CalendarAdapter(days, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+
+    }
+
+    //  This part of the code is for viewing Previous Week from CalendarUtils
+    public void previousWeekAction(View view)
+    {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusWeeks(1);
+        setWeekView();
+    }
+
+    //  This part of the code is for viewing Next Week from CalendarUtils
+    public void nextWeekAction(View view)
+    {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusWeeks(1);
+        setWeekView();
+    }
+
+    //  onItemClick is the selected date in week view
+    @Override
+    public void onItemClick(int position, LocalDate date)
+    {
+        CalendarUtils.selectedDate = date;
+        setWeekView();
+    }
+
+    //  Creation of onResume method
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setEventAdapter();
+    }
+
+    //  Creation of setEventAdapter method
+    private void setEventAdapter()
+    {
+        ArrayList<Event> dailyEvents = Event.eventsForDate(CalendarUtils.selectedDate);
+        EventAdapter eventAdapter = new EventAdapter(getApplicationContext(), dailyEvents);
+        eventListView.setAdapter(eventAdapter);
+    }
+
+    //  Redirects to EventEditActivity
+    public void newEventAction(View view)
+    {
+        startActivity(new Intent(this, EventEditActivity.class));
+    }
+}
